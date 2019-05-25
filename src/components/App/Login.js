055448400064316f3c32993e3b5f9b612a811d94
Stephen Gladney <stephengladney@Stephens-Mac-mini.local>
2019-05-25 16:04:35 -0400

    change Login to component to handle checkbox

--- src/components/App/Login.js
+++ src/components/App/Login.js
@@ -1,88 +1,110 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Login.css";
 import axios from "axios";
 import { setCookie } from "../../wednesday";
 
-const Login = ({ authorizeMe }) => {
-  return (
-    <div>
-      <div className="loginContainer">
-        <img src="/images/wednesday_login.png" className="wednesday" />
-        <div>
-          <h1 id="loginHeader">Not so fast.</h1>
-          <div className="ui inverted segment">
-            <form class="ui inverted form">
-              <div class="field">
-                <label>Username</label>
-                <input
-                  type="text"
-                  id="username"
-                  placeholder=""
-                  style={{
-                    background: "#000000",
-                    color: "#ffffff"
-                  }}
-                />
-              </div>
-              <div class="field">
-                <label>Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder=""
-                  style={{
-                    background: "#000000",
-                    color: "#ffffff"
-                  }}
-                />
-              </div>
-              <div class="field">
-                <div class="ui checkbox">
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rememberMe: false
+    };
+    this.toggleRemember = this.toggleRemember.bind(this);
+    this.authorizeMe = props.authorizeMe;
+  }
+
+  toggleRemember() {
+    this.setState({ rememberMe: !this.state.rememberMe });
+  }
+
+  render() {
+    const checkbox = (
+      <input
+        type="checkbox"
+        tabindex="0"
+        id="rememberMe"
+        checked={this.state.rememberMe}
+        onChange={this.toggleRemember}
+      />
+    );
+
+    return (
+      <div>
+        <div className="loginContainer">
+          <img src="/images/wednesday_login.png" className="wednesday" />
+          <div>
+            <h1 id="loginHeader">Not so fast.</h1>
+            <div className="ui inverted segment">
+              <form class="ui inverted form">
+                <div class="field">
+                  <label>Username</label>
                   <input
-                    type="checkbox"
-                    tabindex="0"
-                    class="hidden"
-                    id="rememberMe"
+                    type="text"
+                    id="username"
+                    placeholder=""
+                    style={{
+                      background: "#000000",
+                      color: "#ffffff"
+                    }}
                   />
-                  <label>Remember me</label>
                 </div>
-              </div>
-              <button
-                class="ui inverted button"
-                onClick={e => {
-                  const usernameSubmitted = document.getElementById("username")
-                    .value;
-                  const pwSubmitted = document.getElementById("password").value;
-                  const rememberMe = document.getElementById("rememberMe")
-                    .checked;
-                  e.preventDefault();
-                  axios({
-                    method: "post",
-                    url: `api/authorize`,
-                    params: {
-                      user: usernameSubmitted,
-                      password: pwSubmitted
-                    },
-                    headers: {}
-                  })
-                    .then(resp => {
-                      if (rememberMe) setCookie("sessionid", "1", 30);
-                      authorizeMe();
+                <div class="field">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder=""
+                    style={{
+                      background: "#000000",
+                      color: "#ffffff"
+                    }}
+                  />
+                </div>
+                <div class="field">
+                  <div class="ui checkbox">
+                    {checkbox}
+                    <label>Remember me</label>
+                  </div>
+                </div>
+                <button
+                  class="ui inverted button"
+                  onClick={e => {
+                    const usernameSubmitted = document.getElementById(
+                      "username"
+                    ).value;
+                    const pwSubmitted = document.getElementById("password")
+                      .value;
+                    const rememberMe = document.getElementById("rememberMe")
+                      .checked;
+                    e.preventDefault();
+                    axios({
+                      method: "post",
+                      url: `api/authorize`,
+                      params: {
+                        user: usernameSubmitted,
+                        password: pwSubmitted
+                      },
+                      headers: {}
                     })
-                    .catch(err => {
-                      document.getElementById("loginHeader").innerHTML =
-                        "Nope.";
-                    });
-                }}
-              >
-                Login
-              </button>
-            </form>
+                      .then(resp => {
+                        if (rememberMe) setCookie("sessionid", "1", 30);
+                        this.authorizeMe();
+                      })
+                      .catch(err => {
+                        document.getElementById("loginHeader").innerHTML =
+                          "Nope.";
+                      });
+                  }}
+                >
+                  Login
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Login;
